7760bd83533b3975c1c9d8ae641edca4534038d5
Nicole Brewer <brewer36@purdue.edu>
2019-12-13 12:25:05 -0500

    add miniconda to macos setup

--- setup-macos.sh
+++ setup-macos.sh
@@ -3,3 +3,12 @@
 # install homebrew package manager
 cd /usr/local
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+
+# install miniconda3
+curl -o https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+
+# this prevents conda from initializing for every shell startup
+conda config --set auto_auctivate_base false
+source ~/.bashrc
+conda install jupyter
+
