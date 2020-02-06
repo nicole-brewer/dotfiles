0bbf3177ad9998daa05b3fdab9cdfc67a69df9a1
Nicole Brewer <brewer36@purdue.edu>
2020-02-06 12:40:20 -0500

    Added ideas in comments for desktop-macos

--- desktop-macos.sh
+++ desktop-macos.sh
@@ -4,3 +4,28 @@ fa_version='5.11.2'
 # install font awesome to macuser fonts
 curl https://use.fontawesome.com/releases/v$fa_version/fontawesome-free-$fa_version-desktop.zip -o /Users/$macuser/Library/Fonts/Font\ Awesome\ 5\ 
 
+
+## what I did the first time
+## perhaps replace with my own exported settings
+# https://www.jetbrains.com/help/idea/sharing-your-ide-settings.html
+
+# install intellij community
+# install typora
+# download solarized
+# rm solarize/intellij
+#git clone https://github.com/jkaving/intellij-colors-solarized.git
+# sudo chmod 755 buildjar.sh
+# ./buildjar.sh
+# file preferences import settings > setting.jar
+# IntelliJIDEA> preferences > editor > color scheme > solarized dark
+
+
+## Save cheatsheet bookmarks to a particular folder and have them all automatically locally downloaded
+# https://www.muhlenberg.edu/media/contentassets/pdf/about/oit/documentation/Bookmarks.pdf
+# https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c689e0bc00ca/conda-cheatsheet.pdf
+
+## download typora
+## write a custom typora theme
+# https://theme.typora.io/doc/Write-Custom-Theme/
+# https://theme.typora.io/doc/Write-Custom-Theme/#test-in-typora
+
